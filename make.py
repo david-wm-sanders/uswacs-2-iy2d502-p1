d44d27cb25e7a087986f05f89aab78e5d63d19da
David Sanders <david.wm.sanders@gmail.com>
2019-03-01 04:56:32 +0000

    Become xelatex for better fontawesome

--- make.py
+++ make.py
@@ -5,9 +5,9 @@ from pathlib import Path
 # .\venv\Scripts\python.exe .\make_full_code_listings.py ..\uswacs-2-iy2d502-salapp\ --not-files=.env,sec_requirements.txt --not-dirs=migrations
 
 print(f"Making report...")
-commands = [["pdflatex.exe", "-shell-escape", "report.tex"],
+commands = [["xelatex.exe", "-shell-escape", "report.tex"],
             ["bibtex.exe", "report.aux"],
-            ["pdflatex.exe", "-shell-escape", "report.tex"],
-            ["pdflatex.exe", "-shell-escape", "report.tex"]]
+            ["xelatex.exe", "-shell-escape", "report.tex"],
+            ["xelatex.exe", "-shell-escape", "report.tex"]]
 for command in commands:
     subprocess.run(command)
